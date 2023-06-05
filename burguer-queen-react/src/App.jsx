4ac48c2b7f14494e6efb91460e6ad6e2c0ca74d5
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-06-05 11:56:10 -0500

    Co-authored-by: Zhaida Cazasola <zka21@users.noreply.github.com>

--- burguer-queen-react/src/App.jsx
+++ burguer-queen-react/src/App.jsx
@@ -1,15 +1,20 @@
-
 import Chef from "./componentes/chef/Chef.jsx";
 import Waiter from "./componentes/waiter/Waiter.jsx";
 import Administrador from "./componentes/administardor/Administrator.jsx";
 import Login from "./componentes/Login.jsx";
+import { useState } from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import './App.css'
+
 function App() {
+  const [token, setToken] = useState('')
+  // funcion de callback que actualizara el token cuando alguien ingresa al sistema
+  const updateToken = (miToken) => setToken(miToken)
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login updateToken={updateToken}/>} />
         <Route path="/waiter" element={<Waiter />} />
         <Route path="/chef" element={<Chef />} />
         <Route path="/administrador" element={<Administrador />} />
--- burguer-queen-react/src/componentes/Login.jsx
+++ burguer-queen-react/src/componentes/Login.jsx
@@ -1,33 +1,61 @@
-import logoBurger from '../assets/logoBurger.png'
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { httpLogin  } from '../helpers/api';
+import logoBurger from "../assets/logoBurger.png";
+import { useState, useEffect } from "react";
+ import { useNavigate } from "react-router-dom";
+import { httpLogin } from "../helpers/api";
+
+const Login = ({ updateToken }) => {
+  //para navegar a rutas
+  const navigate = useNavigate();
+  //Guardar la entrada del usuario  y password
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  const loginClick = async (user, password) => {
+    try {
+      
+      const data = await httpLogin(user, password);
+
+      updateToken(data.accessToken)
+      //segun el rol se navegara
+      if (data.user.role === 'admin') return navigate('/administrador')
+      if (data.user.role === 'chef') return navigate('/chef')
+      if (data.user.role === 'waiter') return navigate('/waiter')
+
+    } catch{
+      console.log('No se encontraron roles asociados a su cuenta ');
+    }
+  };
 
 
-function Login() {
-  
-
+  useEffect(() => updateToken(""), [])
   return (
     <>
-      <main className='lg-main-conteiner'>
-        <div className='lg-logo'>
-          <img src={logoBurger} className='lg-logo-img' alt='Logo Burger Queen' />
+      <main className="lg-main-conteiner">
+        <div className="lg-logo">
+          <img
+            src={logoBurger}
+            className="lg-logo-img"
+            alt="Logo Burger Queen"
+          />
         </div>
-        <div className='lg-form-container'>
-          <form action="" className='lg-form'>
+        <div className="lg-form-container">
+          <form action="" className="lg-form">
             <h2>WELCOME</h2>
             <label htmlFor="email">Email</label>
-            <input type="email" placeholder='example@gmail.com' />
+            <input type="email" placeholder="example@gmail.com" 
+            value={user} 
+            onChange={(e) => setUser(e.target.value)}/>
             <label htmlFor="password">Password</label>
-            <input type="password" placeholder='Enter Password' />
-            <button>Login</button>
+            <input type="password" placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+            />
+            <button onClick={loginClick(user, password)}>Login</button>
           </form>
         </div>
       </main>
-
     </>
-  )
+  );
 }
 
-export default Login
-
+export default Login;
