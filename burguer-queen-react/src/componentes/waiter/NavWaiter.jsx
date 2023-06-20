4a7792482cf9c78e98098442a39d91f5b7ed325d
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-06-19 19:51:17 -0500

    corrigiendo redirecciones

--- burguer-queen-react/src/componentes/waiter/NavWaiter.jsx
+++ burguer-queen-react/src/componentes/waiter/NavWaiter.jsx
@@ -13,11 +13,15 @@ export const NavWaiter  = () => {
             </figure>
             <nav>  
               <ul>
-                 <Link className="nav-link" to={`/waiter/orders`}>NEW ORDER </Link>
-                 <Link className="nav-link" to={`/waiter/orders/list`}>ORDER LIST </Link>
+                 <Link className="nav-link" to="/waiter/orders">NEW ORDER </Link>
+                 <Link className="nav-link" to="/waiter/orders/list">ORDER LIST </Link>
               </ul>
             </nav>
-              <button className='waiter-btn'><FontAwesomeIcon icon={faRightFromBracket} size="lg" style={{color: "#e1d76b",}}></FontAwesomeIcon></button>
+            <Link to="/" className="salir-button">
+    <button className="waiter-btn">
+      <FontAwesomeIcon icon={faRightFromBracket} size="lg" style={{ color: "#e1d76b" }} />
+    </button>
+  </Link>
          </header>
     </>
     )
