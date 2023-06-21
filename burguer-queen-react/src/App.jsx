59fb7ca43a52af20bcf89d5dd6c7468bda82711d
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-06-21 17:06:31 -0500

    update with comments

--- burguer-queen-react/src/App.jsx
+++ burguer-queen-react/src/App.jsx
@@ -7,24 +7,39 @@ import ListOrders from "./componentes/waiter/OrderList.jsx";
 import { useState } from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
-
+// Función principal del componente App
 function App() {
-  const [token, setToken] = useState('')
-  // funcion de callback que actualizara el token cuando alguien ingresa al sistema
-  const updateToken = (miToken) => setToken(miToken)
+  const [token, setToken] = useState('');
+  // Estado para almacenar el token
+
+  // Función de callback que actualiza el token cuando alguien ingresa al sistema
+  const updateToken = (miToken) => setToken(miToken);
 
   return (
     <Router>
+      {/* Configuración de las rutas */}
       <Routes>
+        {/* Ruta para la página de inicio de sesión */}
         <Route path="/" element={<Login updateToken={updateToken}/>} />
+
+        {/* Ruta para la página de órdenes del mesero */}
         <Route path="/waiter/orders" element={<Waiter token={token} />} />
+
+        {/* Ruta para la página del chef */}
         <Route path="/chef" element={<Chef />} />
+
+        {/* Ruta para la página de empleados del administrador */}
         <Route path="/administrador/employed" element={<Administrador token={token} />} />
+
+        {/* Ruta para la página de lista de órdenes del mesero */}
         <Route path="/waiter/orders/list" element={<ListOrders  token={token} />} />
+
+        {/* Ruta para la página de lista de órdenes del chef */}
         <Route path="/chef/orders/list" element={<ListOrden token={token}/>} />
       </Routes>
     </Router>
   );
 }
+
 
 export default App;
