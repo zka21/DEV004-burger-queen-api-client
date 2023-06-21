6448678f5c0b3d4aa358f5a3b1c6ad232b0ca8d3
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-06-21 17:08:07 -0500

    update

--- burguer-queen-react/src/main.jsx
+++ burguer-queen-react/src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
+// Crea un punto de montaje en el elemento HTML con id 'root' y renderiza el componente <App />
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-)
+  );
+  
