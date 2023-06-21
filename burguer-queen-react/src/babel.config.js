a7b4af41de31620d2a5fc08449ba796465e3b810
ZhaidaCazasola <zhaidacazasola.20@gmail.com>
2023-06-21 13:40:11 -0500

    avance contiene error en pruebas con jest

--- burguer-queen-react/src/babel.config.js
+++ burguer-queen-react/src/babel.config.js
@@ -2,11 +2,7 @@ export default {
     presets: [
       [
         '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
+        '@babel/preset-react'
       ],
     ],
   };
--- burguer-queen-react/src/jest.config.js
+++ burguer-queen-react/src/jest.config.js
@@ -1,4 +1,6 @@
 export default {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFiles: ['./jest.setup.js']
-}
+  testEnvironment: 'jsdom',
+  transform: {
+      '^.+\\.jsx?$': 'babel-jest', // transpile mjs, mts, js, ts files
+  },
+};
